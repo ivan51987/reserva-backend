16e756a2e2b3fba726e47a05e29c23617e141d9e
Ivan Flores Flors <ivanflores51987@gmail.com>
2025-03-23 18:41:01 -0400

    subir su configuracion

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ const routes = require('./routes');
 const app = express();
 
 // Configuración
-app.use(cors());
+//app.use(cors());
+
+app.use(cors({
+  origin: ['https://reserva-frontend.onrender.com'], // Ajusta con tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Conectar a MongoDB (asegúrate de cambiar la URL)
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('Conectado a MongoDB'))
-  .catch(err => console.error('Error al conectar a MongoDB:', err));
+//mongoose.connect(process.env.MONGO_URI).then(() => console.log('Conectado a MongoDB'))
+//  .catch(err => console.error('Error al conectar a MongoDB:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 5000 // Evita timeout prolongado
+})
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error('Error al conectar a MongoDB:', err));
 
 app.use(routes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+//app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+
+app.listen(PORT, () => console.log(`Servidor en https://reserva-backend.onrender.com`));
